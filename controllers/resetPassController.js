93562d68e4966d7bf0b988ffcabdfd1b1aa94a59
Andres Breuer <breuer.andres@gmail.com>
2022-07-15 16:36:32 -0300

    update de reset pwd

--- controllers/resetPassController.js
+++ controllers/resetPassController.js
@@ -3,17 +3,27 @@ const bcrypt = require ('bcrypt');
 
 const resetPass = async (req, res) => {
 
-    const { email, pwd } = req.body
+    const { email, pwd, oldPwd } = req.body
+
+    if(req.body.oldPwd){
+        const foundUser = await User.findOne({ email: email }).exec();
+        const match = await bcrypt.compare(oldPwd, foundUser.pwd)
+        console.log("1");
+        if (!match){
+            return res.status(409)
+        }
+        
+    }
 
     try {
         const hashedPwd = await bcrypt.hash(pwd, 10);
-
+        console.log("2");
         const found = User.findOneAndUpdate({ email: email }, { pwd: hashedPwd}).exec()
         if (!found) 
             {return res.sendStatus(409);
         }
         else {
-            return res.status(201).json({'success': 'success'})
+            return res.status(201)
 
         }
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,11 @@ const User = require('../model/User');
 
 const modifyUser = (req,res) => {
 
-    const { firstName, lastName, email } = req.body;
+    const { firstName, lastName, email, phone } = req.body;
 
     try {
         
-        const found = User.findOneAndUpdate({ email: email }, { firstName: firstName, lastName: lastName}).exec()
+        const found = User.findOneAndUpdate({ email: email }, { firstName: firstName, lastName: lastName, phone: phone}).exec()
         if (!found) 
             {return res.sendStatus(409);
         }
